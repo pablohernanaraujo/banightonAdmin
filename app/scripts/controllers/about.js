72bc29364ed13e7ffc8976bd5aa5a56612da39a5
Pablo Hernán Araujo <pablohernanaraujo@gmail.com>
2015-11-20 19:02:03 -0300

    inicio administrador

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name banightonAdminApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the banightonAdminApp
+ */
+angular.module('banightonAdminApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name banightonAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the banightonAdminApp
+ */
+angular.module('banightonAdminApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
