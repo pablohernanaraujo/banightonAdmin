c1aa28569fc5842c29e79c6b820ac89f7a92c8ac
pablohernanaraujo <pablohernanaraujo@gail.com>
2016-01-05 00:44:20 -0300

    administrador grunt build sin errores

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -39,10 +39,11 @@ module.exports = function(config) {
       'bower_components/firebase/firebase.js',
       'bower_components/angularfire/dist/angularfire.js',
       'bower_components/ng-file-model/ng-file-model.js',
-      'bower_components/si-table/dist/si-table.js',
       'bower_components/bootstrap-table/src/bootstrap-table.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-      'bower_components/angular-busy/dist/angular-busy.js',
+      'bower_components/angular-simple-logger/dist/angular-simple-logger.js',
+      'bower_components/lodash/lodash.js',
+      'bower_components/angular-google-maps/dist/angular-google-maps.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
